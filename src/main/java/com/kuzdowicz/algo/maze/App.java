f5813a812878d7115936d89b190a9dd03e2c4c39
Martin Kuzdowicz <m.kuzdowicz@elsevier.com>
2018-03-05 00:06:11 +0000

    wip

--- src/main/java/com/kuzdowicz/algo/maze/App.java
+++ src/main/java/com/kuzdowicz/algo/maze/App.java
@@ -9,18 +9,22 @@ public class App {
     public static void main(String[] args) {
 
         List<List<String>> maze1 = new MazeLoader().loadMazeFromFile("small_maze.txt");
-        System.out.println(maze1);
+        println(maze1);
         boolean solved = new MazeSolver(maze1, false).solve();
-        System.out.println("SOLVED maze1 => " + solved);
+        println("SOLVED maze1 => " + solved);
 
         List<List<String>> maze2 = new MazeLoader().loadMazeFromFile("small_maze_no_solution.txt");
-        System.out.println(maze2);
+        println(maze2);
         boolean solved2 = new MazeSolver(maze2, false).solve();
-        System.out.println("SOLVED2 => " + solved2);
+        println("SOLVED2 => " + solved2);
 
         boolean solved3 = new MazeSolver(maze2, true).solve();
-        System.out.println("SOLVED3 with ability to move diagonal => " + solved3);
+        println("SOLVED3 with ability to move diagonal => " + solved3);
 
+    }
+
+    private static void println(Object o) {
+        System.out.println(o);
     }
 
 }
